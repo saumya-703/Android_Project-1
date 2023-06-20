c8fc7e9c4dd10e1c16329b854683baf9107bcc8c
saumya <verma.saumya.703@gmail.com>
2023-06-20 23:28:08 +0530

    Intial Commit 1

--- app/src/main/java/com/project/helloworld/MainActivity.java
+++ app/src/main/java/com/project/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.project.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,5 +18,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         i1=findViewById(R.id.i1);
         i2=findViewById(R.id.i2);
+        add=findViewById(R.id.add);
+        sub=findViewById(R.id.sub);
+        mul=findViewById(R.id.mul);
+        div=findViewById(R.id.div);
+        result=findViewById(R.id.result);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,c;
+                a=Integer.parseInt(i1.getText().toString());
+                b=Integer.parseInt(i2.getText().toString());
+                c=a+b;
+                result.setText("Result = "+c);
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,c;
+                a=Integer.parseInt(i1.getText().toString());
+                b=Integer.parseInt(i2.getText().toString());
+                c=a-b;
+                result.setText("Result = "+c);
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,c;
+                a=Integer.parseInt(i1.getText().toString());
+                b=Integer.parseInt(i2.getText().toString());
+                c=a*b;
+                result.setText("Result = "+c);
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,c;
+                a=Integer.parseInt(i1.getText().toString());
+                b=Integer.parseInt(i2.getText().toString());
+                c=a/b;
+                result.setText("Result = "+c);
+            }
+        });
     }
 }
